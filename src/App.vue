e5f7894f022376de3973ed2b0c953a090ea3cc63
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-12-27 16:16:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavbarHeader />
+</template>
+
+<script>
+import NavbarHeader from "./components/NavbarHeader.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: { NavbarHeader },
+};
+</script>
+
+<style lang="scss" scoped></style>
